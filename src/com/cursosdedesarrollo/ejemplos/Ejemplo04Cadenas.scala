b7ef821bd956e16920168bf9c4e8fec1db9ab7bc
David Vaquero <pepesan@gmail.com>
2018-06-17 23:30:50 +0200

    ejemplos de cadenas arrays colecciones tuplas

---
+++ src/com/cursosdedesarrollo/ejemplos/Ejemplo04Cadenas.scala
@@ -0,0 +1,30 @@
+package com.cursosdedesarrollo.ejemplos
+
+object Ejemplo04Cadenas {
+  def main(args: Array[String]) {
+    var cadena = "Hola Mundo"
+    //longitud
+    print(cadena.length)
+    //minúsculas
+    print(cadena.toLowerCase)
+    //mayúsculas
+    print(cadena.toUpperCase)
+    var string1 = "Hola "
+    var string2 ="Mundo!"
+    println(string1.concat(string2))
+
+
+    println("My name is ".concat("Zara"))
+
+    println("Hello," + " world" + "!")
+
+
+    val name = "James"
+    println(s"Hello, $name") //output: Hello, James
+
+    val height = 1.9d
+    println(f"$name%s is $height%2.2f meters tall") //James is 1.90 meters tall
+
+    println(raw"Result = \n a \n b")
+  }
+}
---
+++ src/com/cursosdedesarrollo/ejemplos/Ejemplo06Colleciones.scala
@@ -0,0 +1,75 @@
+package com.cursosdedesarrollo.ejemplos
+
+object Ejemplo06Colleciones {
+  def main(args: Array[String]) {
+    // Listado de cadenas
+    var frutas: List[String] = List("manzanas", "naranjas", "peras")
+    println( "Cabecera : " + frutas.head )
+    println( "Cola : " + frutas.tail )
+    println( "Comprueba si está vacío : " + frutas.isEmpty)
+    println(frutas(1))
+    println(frutas)
+    frutas = "uvas" :: frutas
+    println(frutas)
+    frutas = frutas ::: List("melones")
+    println(frutas)
+
+    frutas.foreach({
+      println
+    })
+
+    for (fruta <- frutas){
+      println(fruta)
+    }
+    // Listado de dos dimensiones
+    var dim: List[List[Int]] =
+      List(
+        List(1, 0, 0),
+        List(0, 1, 0),
+        List(0, 0, 1)
+      )
+    println(dim)
+    for (fila <- dim){
+      println(fila)
+      for (col <- fila){
+        println(col)
+      }
+    }
+
+    var frutas2 = List.fill(3)("peras")
+    println(frutas2)
+
+    var x = List(5, 1, 4, 3, 2)
+    x = x.filter(_ > 2)
+    println(x)
+
+    var listado = List(1,2,3)
+    var listadoMapeado = listado.map(a => a * 2)
+    println(listadoMapeado)
+
+
+    var nombres = List("Fred", "Joe", "Bob")
+    val minus = nombres.map(_.toLowerCase)
+    println(minus)
+
+
+    var mapa:Map[Char,Int] = Map()
+
+    mapa += ('I' -> 1)
+    mapa += ('J' -> 5)
+    mapa += ('K' -> 10)
+    mapa += ('L' -> 100)
+    println(mapa)
+    println(mapa.keys)
+    println(mapa.values)
+    println(mapa.isEmpty)
+    println(mapa.get('I'))
+    mapa.keys.foreach{ i =>
+      print( "Clave = " + i )
+      println(" Valor = " + mapa(i) )}
+
+
+
+
+  }
+}
